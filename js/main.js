9c190e6165bd947f9ee973852d319bc19c8aa0bc
Kr1SEl <k.kostakov2002@gmail.com>
2025-01-12 22:04:33 +0100

    Images init

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
-function injectHeader(){
-    fetch('/Yachting/components/shared/header.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('header').innerHTML = data);
+async function injectHeader(){
+    const headerContainer = document.getElementById('header');
+    try {
+        const response = await fetch('/on-the-wave/components/shared/header.html');
+        if (response.ok) {
+            headerContainer.innerHTML = await response.text();
+            injectNavbar();
+        } else {
+            console.error('Failed to load header:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error loading header:', error);
+    }
+}
+
+function injectNavbar(){
+    const navbar = document.getElementById('navbar');
+
+    window.addEventListener('scroll', () => {
+        if (navbar){
+            if (window.scrollY > 10) {
+                navbar.classList.remove('navbar-transparent');
+                navbar.classList.add('navbar-solid');
+            } else {
+                navbar.classList.remove('navbar-solid');
+                navbar.classList.add('navbar-transparent');
+            }
+        }
+    });
 }
 
 function injectFooter(){
-    fetch('/Yachting/components/shared/footer.html')
+    fetch('/on-the-wave/components/shared/footer.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('footer').innerHTML = data);
+}
+
+function injectFooter() {
+    fetch('/on-the-wave/components/shared/footer.html')
         .then(response => response.text())
         .then(data => document.getElementById('footer').innerHTML = data);
 }
@@ -13,4 +44,7 @@ function injectFooter(){
 document.addEventListener('DOMContentLoaded', () => {
     injectHeader();
     injectFooter();
+    injectNavbar();
 });
+
+
